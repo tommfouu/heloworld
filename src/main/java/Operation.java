b0b109de30ebb23cbbbbcca7b0fce6d85a14ff9a
Tom MFOU'OU <tom.mfou-ou-mvondo@univ-nantes.fr>
2020-10-28 11:29:46 +0100

    frst commit

---
+++ src/main/java/Operation.java
@@ -0,0 +1,7 @@
+
+public class Operation {
+
+	public static int addition(int a, int b) {
+		return a+b;
+	}
+}
---
+++ src/test/java/TestAddition.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAddition {
+
+	@Test
+	public void test() {
+		assertEquals(15,Operation.addition(5, 10));
+	}
+
+}
