5c8e2f55534fb6f318eb65cb7b4a7c0c7332a153
tommfouu <55479594+tommfouu@users.noreply.github.com>
2021-01-12 10:10:17 +0100

    Update TestAddition.java

--- src/test/java/TestAddition.java
+++ src/test/java/TestAddition.java
@@ -8,5 +8,10 @@ public class TestAddition {
 	public void test() {
 		assertEquals(15,Operation.addition(5, 15));
 	}
+	
+	@Test
+	public void test2() {
+		assertEquals(15,Operation.addition(5, 10));
+	}
 
 }
