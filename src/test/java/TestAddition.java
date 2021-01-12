1c6fe05c43fc10c7be285c9169ad5da3137a2438
tommfouu <55479594+tommfouu@users.noreply.github.com>
2021-01-12 13:53:27 +0100

    Update TestAddition.java

--- src/test/java/TestAddition.java
+++ src/test/java/TestAddition.java
@@ -3,11 +3,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestAddition {
-
-	@Test
-	public void test() {
-		assertEquals(15,Operation.addition(5, 15));
-	}
 	
 	@Test
 	public void test2() {
